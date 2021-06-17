e2bbdcd07397358b20421d3060e73064914b413e
krsailesh <krsailesh2001@gmail.com>
2021-06-18 00:32:49 +0530

    req.query challenge implemented

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { response } = require('express');
 
 mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log('database connected...'))
@@ -28,10 +29,7 @@ const userSchema = new mongoose.Schema({
       type: Number,
       required: true
     },
-    date: {
-      type: Date,
-      default: Date.now
-    }
+    date: String
   }]
 })
 
@@ -74,11 +72,18 @@ app.post('/api/users/:_id/exercises', (req,res) => {
     return res.json(`Path ${description?'duration':'description'} is required.`);
   }
   if(!date){
-    date = new Date();
+    date = new Date().toDateString();
+  }else{
+    if(new Date(date).toDateString()=='Invalid Date'){
+      return res.json(`Cast to date failed for value ${date} at path date`)
+    }
+
+    date = new Date(date).toDateString();
   }
   if(duration.match(/[^0-9]/g)){
     return res.json(`Cast to Number failed for value ${duration} at path duration`)
   }
+  duration = parseInt(duration);
   User.findByIdAndUpdate(
     _id,
     {$push: {"log": {description, duration, date}}, $inc: { count: 1 }},
@@ -102,16 +107,63 @@ app.post('/api/users/:_id/exercises', (req,res) => {
 
 app.get('/api/users/:_id/logs', (req,res) => {
   const { _id } = req.params;
+
   User.findById(_id)
     .then((data) => {
       if(!data){
         return res.json('Unkown userId');
       }
-
-      return res.json(data);
+      if(!Object.keys(req.query).length){
+        return res.json(data);
+      }
+      
+      const { from, to } = req.query;
+      const logArray = filterTheLogArray(data.log,req.query);
+      let responseObj = {_id: data._id, 
+                          username: data.username, 
+                          count: logArray.length, 
+                          log: logArray };
+      
+      if(from){
+        responseObj = {...responseObj, from: new Date(from).toDateString()};
+      }
+      if(to){
+        responseObj = {...responseObj, to: new Date(to).toDateString()};
+      }
+      return res.json(responseObj);
     })
     .catch(err => console.log(err));
 })
+
+const filterTheLogArray = (logArray, queryParam) => {
+  let newArray = logArray;
+  if(queryParam.from){
+    newArray = logArray.filter((data) => {
+      return new Date(queryParam.from) <= new Date(data.date);
+    })
+  }
+
+  if(queryParam.to){
+    newArray = logArray.filter((data) => {
+      return new Date(queryParam.to) >= new Date(data.date);
+    })
+  }
+  
+  if(queryParam.limit < newArray.length){
+    newArray = newArray.slice(0,queryParam.limit);
+  }
+  
+  let arrWithoutId = [];
+  newArray.forEach((data) => {
+    arrWithoutId.push({
+      description: data.description,
+      duration: data.duration,
+      date: data.date
+    })
+  })
+  return arrWithoutId;
+}
+
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
